b4635288639dede07bcdb2f00bba50c5da8be6f4
Md-shefat-masum <mshefat924@gmail.com>
2022-11-27 22:46:49 +0600

    change in profile blade

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,6 +5,7 @@
             <div class="card">
                 <div class="card-header">
                     <h1>User Profile</h1>
+                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos sint aliquam quod doloribus nobis consequuntur eligendi hic error odit magni saepe suscipit cumque iure incidunt quidem ipsum, labore officiis cum!</h2>
                 </div>
                 <div class="card-body">
                     <img width="100" src="{{ asset(auth()->user()->photo) }}" alt="">
